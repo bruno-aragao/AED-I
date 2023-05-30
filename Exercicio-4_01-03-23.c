2e40a9062c3c84f07e1f5e4dcab32da736c395a6
bruno-aragao <105687878+bruno-aragao@users.noreply.github.com>
2023-05-30 19:06:35 -0300

    Add files via upload

---
+++ Exercicio-4_01-03-23.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+lISTA 2 - EXERCÍCIO 4
+          A) imprima o maior elemento 
+          B) imprima a posição do maior elemento
+
+******************************************************************************/
+#include <stdio.h>
+#define TAM 10
+int main()
+{
+    int v[TAM], maior, posicao;
+    // entrada
+    for (int i=0; i<TAM; i++) {
+        printf("Digite um valor v[%d]: ", i);
+        scanf("%d", &v[i]);
+    }
+    // processamento
+    for (int i=0; i<TAM; i++) {
+        if(v[i]>maior){
+            maior = v[i];
+            posicao = i++;
+        }
+    }
+    printf("\nO maior elemento é %d e esta na posição %d", maior, posicao);
+
+    return 0;
+}
+
